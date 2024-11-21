a7fef42f0983fbc63b3d5fd369e5f1d77c6b426f
Gilang Swandaru <gilangswandaru27@gmail.com>
2024-11-22 00:30:22 +0800

    feat: restructure project folders, install Gin framework, and implement simple REST API
    - Restructured the project folders to align with Go best practices.
    - Installed the Gin framework for building the REST API.
    - Created a simple REST API with modular folder organization based on functionality.

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/glng-swndru/simple-forum/internal/handlers/memberships"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+
+	membershipHandler := memberships.NewHandler(r)
+	membershipHandler.RegisterRoute()
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
