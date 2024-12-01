593f0da357fe87ba9b98fda0d771a3a882ae84f6
Gilang Swandaru <gilangswandaru27@gmail.com>
2024-12-02 07:21:56 +0800

    feat: implement create comment functionality
    - Added `CreateCommentRequest` model for client input and `CommentModel` for database representation
    - Developed repository to handle comment creation
    - Added service to connect to the create comment repository
    - Implemented handler for creating comments and integrated with the service
    - Added route path for creating comments

--- internal/handlers/posts/handler_posts.go
+++ internal/handlers/posts/handler_posts.go
@@ -10,6 +10,7 @@ import (
 
 type postService interface {
 	CreatePost(ctx context.Context, UserID int64, req posts.CreatePostRequest) error
+	CreateComment(ctx context.Context, postID, userID int64, request posts.CreateCommentRequest) error
 }
 type Handler struct {
 	*gin.Engine
@@ -29,4 +30,5 @@ func (h *Handler) RegisterRoute() {
 	route.Use(middleware.AuthMiddleware())
 
 	route.POST("/create", h.CreatePost)
+	route.POST("/comment/:postID", h.CreateComment)
 }
